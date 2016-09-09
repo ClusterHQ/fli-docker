a2c0824b0f7f158b848b1250cbb7608362981f77
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-09-09 14:24:59 -0400

    check for compose and dpcli

--- cmd/fs3docker/main.go
+++ cmd/fs3docker/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"flag"
-	_ "github.com/wallnerryan/fs3todocker/utils"
+	"github.com/wallnerryan/fs3todocker/utils"
 )
 
 func main() {
@@ -13,6 +14,38 @@ func main() {
     var manifest string
     var composeOpts string 
 
+    // `docker-compose` path
+    // TODO configurable in the future
+    var composePath string
+    composePath = "/usr/local/bin/docker-compose"
+
+    // `dpcli` Path
+    // TODO maybe configurable in the future
+    var dpcliPath string
+    dpcliPath = "/opt/clusterhq/bin/dpcli"
+
+    // Check if needed dependencies are available
+    isComposeAvail, err := utils.CheckForTool(composePath)
+    if (!isComposeAvail){
+    	fmt.Printf("-----------------------------------------------------------------------\n")
+    	fmt.Printf("docker-compose is not installed, it is needed to use fs3todocker\n")
+		fmt.Printf("docker-compose is available at https://docs.docker.com/compose/install/\n")
+		fmt.Printf("-----------------------------------------------------------------------\n")
+		log.Fatal(err.Error())
+    }else{
+		log.Println("docker-compose Ready!\n")
+    }
+
+    isDpcliAvail, err := utils.CheckForTool(dpcliPath)
+    if (!isDpcliAvail){
+    	fmt.Printf("-------------------------------------------------------\n")
+    	fmt.Printf("dpcli is not installed, it is needed to use fs3todocker\n")
+		fmt.Printf("dpcli is available at https://clusterhq.com\n")
+		fmt.Printf("-------------------------------------------------------\n")
+		log.Fatal(err.Error())
+    }else{
+		log.Println("dpcli Ready!\n")
+    }
 
     flag.StringVar(&user, "u", "", "Flocker Hub username")
     flag.StringVar(&token, "t", "", "Flocker Hub user token")
--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,16 @@
 package utils
+
+import (
+	"os/exec"
+	"log"
+)
+
+
+func CheckForTool(cliPath string) (result bool, err error) {
+	path, err := exec.LookPath(cliPath)
+	if err != nil {
+		return false, err
+	}
+	log.Println("Found path: " + path + "\n")
+    return true, nil
+}

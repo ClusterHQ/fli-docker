25c1976ace473980c2fb08a7fc8f732f100f42ee
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-10-29 11:48:00 -0400

    typo

--- build_test.sh
+++ build_test.sh
@@ -8,4 +8,8 @@ go test -v ./...
 go build -v ./...
 go install -v ./...
 mv $GOPATH/bin/fli-docker build/
-curl -T build/fli-docker -u${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/chqtest/fli-docker/${PACKAGE_NAME}/${VERSION{/${FILE_TARGET_PATH}
+
+if ! $TRAVIS_PULL_REQUEST
+	then
+		curl -T build/fli-docker -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/chqtest/fli-docker/$PACKAGE_NAME/$VERSION/$FILE_TARGET_PATH
+fi

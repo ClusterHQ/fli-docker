6463dbc3a2efbc9a9ff713e3aac05c666819826d
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-10-31 16:55:11 -0400

    upload to s3

--- build_test.sh
+++ build_test.sh
@@ -1,16 +1,56 @@
 #!/bin/bash
 
-PACKAGE_NAME="fli-docker"
-VERSION="0.0.0-dev"
-FILE_TARGET_PATH="fli-docker"
+BUCKET_NAME="clusterhq-fli-docker"
+VERSION="0.0.2-dev"
 
-go test -v ./...
-go build -v ./...
-go install -v ./...
-mv $GOPATH/bin/fli-docker build/
+function PreflightUbuntu
+{
+  apt-get update -y
+  apt-get install -y python3
+  pip3 install awscli
 
-if ! $TRAVIS_PULL_REQUEST && [ $TRAVIS_BRANCH == "master" ]; then
-	curl -T build/fli-docker -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/chqtest/fli-docker/$PACKAGE_NAME/$VERSION/$FILE_TARGET_PATH
-else
-	echo "Skipping push of version $VERSION, not master branch"
-fi
+}
+
+function SetupAWSCredentials
+{
+  if ! test -d ${HOME}/.aws; then
+    mkdir ${HOME}/.aws
+  fi
+  printf "$(cat .aws-cred-template)" "$AWS_SECRET_ID" "$AWS_ACCESS_KEY" > ${HOME}/.aws/credentials
+}
+
+### Upload HTML to Amazon S3
+function UploadToS3
+{
+  SetupAWSCredentials
+  aws s3 sync build/bin/ s3://$BUCKET_NAME/$VERSION
+}
+
+
+function BuildAndTest
+{
+  go test -v ./...
+  go build -v ./...
+  go install -v ./...
+  mkdir build/bin
+  mv $GOPATH/bin/fli-docker build/bin/
+}
+
+function Main
+{
+  BuildAndTest
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    PreflightUbuntu
+  else
+    echo "Unrecognized operating system"
+    exit 1
+  fi
+
+  if ! $TRAVIS_PULL_REQUEST && [ $TRAVIS_BRANCH == "s3" ]; then
+	  UploadToS3
+  else
+	  echo "Skipping push of version $VERSION, not master branch"
+  fi
+
+Main

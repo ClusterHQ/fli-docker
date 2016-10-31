cbfac96073d165f3439b956e418a3c092a010b67
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-10-31 17:18:14 -0400

    pip

--- build_test.sh
+++ build_test.sh
@@ -2,10 +2,11 @@
 
 BUCKET_NAME="clusterhq-fli-docker"
 VERSION="0.0.2-dev"
+UPLOAD_ON_BRANCH="0-0-2-dev"
 
 function PreflightUbuntu
 {
-  pip3 install awscli
+  pip install awscli
 }
 
 function SetupAWSCredentials
@@ -44,7 +45,8 @@ function Main
     exit 1
   fi
 
-  if ! $TRAVIS_PULL_REQUEST && [ $TRAVIS_BRANCH == "s3" ]; then
+  if ! $TRAVIS_PULL_REQUEST && [ $TRAVIS_BRANCH == "$UPLOAD_ON_BRANCH" ]; then
+      # make sure UPLOAD_ON_BRANCH and VERSION are set above.
 	  UploadToS3
   else
 	  echo "Skipping push of version $VERSION, not master branch"

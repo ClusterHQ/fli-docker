9747ed3c8de3dbf86be3d004d852a87ae27da86d
Ryan Wallner <ryan.wallner@clusterhq.com>
2016-11-01 11:58:47 -0400

    for 0.0.4-dev

--- utils/utils.go
+++ utils/utils.go
@@ -39,7 +39,7 @@ fli is available at https://clusterhq.com
 Using the fli contianer? Make sure it used docker tag 'clusterhq/fli'
 -------------------------------------------------------`
 
-var FliDockerVersion = `Version: v0.0.1-dev`
+var FliDockerVersion = `Version: v0.0.4-dev`
 
 var FliDockerHelp = `
 Usage:
